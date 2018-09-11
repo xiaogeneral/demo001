b2b02a3661fa4ba2c09c5ae0dee074433513f8b5
杨东亮 <yangdl4@lenovo.com>
2018-09-11 11:20:03 +0800

    add component test001.vue

---
+++ src/components/test001.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="main">
+      <h3>{{name}}</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: "test001",
+      data() {
+        return {
+          list: null,
+          name: 'xiaojiang'
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
